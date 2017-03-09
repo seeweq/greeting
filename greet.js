ed281edb546c45318eafaba583d714d9ded1112a
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-03-09 15:52:01 +0200

    changes for function,add reset button

--- greet.js
+++ greet.js
@@ -14,42 +14,52 @@ var radiobton = document.getElementById('radiobton');
 var textBox = document.getElementById('gveText');
 var demo = document.getElementById('demo');
 var display =document.getElementById('count')
-var resetbto=document.getElementById('resetbton');
+
 var lang=' ';
 var count=0;
 
-var pplgreeted = {};
-var pplgreeted = [];
+var namesGreeted = {}
+for (var i = 0; i < namesGreeted.length; i++) {
+
+}
 
 radiobton.addEventListener("change" ,function(e){
 lang = e.target.value;
 });
-button.addEventListener("click", function() {
+button.addEventListener("click", function(){
 var input = textBox.value;
-if(lang==="english" && input.length > 0){
+
+if (lang ==="english" && input.length > 0 && namesGreeted[i] === undefined){
 demo.innerHTML="Hello, " + input;
-// console.log(textBox.length)
+namesGreeted[i] = 1;
 count++;
 display.innerHTML = count
 }
-if(lang ==="isiXhosa" && input.length > 0){
+if(lang ==="isiXhosa" && input.length > 0 && namesGreeted[i] === undefined){
 demo.innerHTML="Molo, " + input;
+namesGreeted[i] = 1;
 count++;
 display.innerHTML = count
 }
-if(lang === "afrikaans" && input.length > 0){4
+if(lang === "afrikaans" && input.length > 0 && namesGreeted[i] === undefined){
 demo.innerHTML="Halo, " + input;
+namesGreeted[i] = 1;
 count++;
 display.innerHTML = count
 }
 localStorage.setItem('count', count);
 //window.alert(localStorage.count);
-document.getElementById('gveText').value ='';
+document.getElementById('gveText').value =''
 
 });
 
 var count=localStorage.getItem('count');
-document.getElementById("count").innerHTML =  count;
-resetbto.addEventListener("click", function(){
-display.innerHTML= 0;
-});
+document.getElementById("count").innerHTML = count;
+
+function resetBtn(){
+  var resetbto=document.getElementById('count');
+  if(resetbto >= 0){
+    document.getElementById('count').innerHTML = count = 0;
+  }
+  localStorage.clear();
+}
